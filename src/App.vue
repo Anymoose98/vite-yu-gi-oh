9bf5430fb14df32b73bc9e619f8afe2494f2d1b5
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-01-11 17:24:42 +0100

    aggiunto e collegato scss e bootstrap

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,6 @@ export default {
 </template>
 
 
-<style lang="">
-  
+<style lang="scss">
+@use './style/generals.scss';
 </style>
--- src/AppHeader.vue
+++ src/AppHeader.vue
@@ -6,6 +6,7 @@ export default {
 
 <template lang="">
   <div>
+    <img src="https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABdpYalFU9SD9K6dNwu9SlKsNaQ3AIGWFrN_uDy6R7N3TS5DHIatIETtgeqwC0kM01zJyor2znGql_ixTWzVueA-GllV7Hn03ygETN_Nd7oZA.png?r=81f" alt="">
     <h2>PROVA PROVA</h2>
   </div>
 </template>
