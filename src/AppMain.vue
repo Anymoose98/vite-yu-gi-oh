3c6398271b7eab3fd74ac0c23fc7bc45a796493f
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-01-11 18:26:28 +0100

    Creato la card base e verificato esteticamente come stanno

--- src/AppMain.vue
+++ src/AppMain.vue
@@ -6,7 +6,13 @@ export default {
 
 <template lang="">
     <main>
-        
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
     </main>
 </template>
 
@@ -14,7 +20,17 @@ export default {
 main {
     max-width: 1200px;
     margin: 30px auto;
-    height: 600px;
     background-color: white;
+    display: flex;
+    flex-wrap: wrap;
+
+    .card {
+        width: calc(100% / 5 - 10px);
+        margin: 5px;
+        background-color: orange;
+        height: 300px;
+
+    }
+
 }
 </style>
