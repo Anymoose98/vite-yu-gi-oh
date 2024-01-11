b93a6b5f0e22816e96dbbb478064d142442aa4ce
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-01-11 17:42:28 +0100

    AppHeader finita

--- src/AppHeader.vue
+++ src/AppHeader.vue
@@ -5,15 +5,26 @@ export default {
 </script>
 
 <template lang="">
-  <div>
+  <header class="p-3">
     <img src="https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABdpYalFU9SD9K6dNwu9SlKsNaQ3AIGWFrN_uDy6R7N3TS5DHIatIETtgeqwC0kM01zJyor2znGql_ixTWzVueA-GllV7Hn03ygETN_Nd7oZA.png?r=81f" alt="">
-    <h2>PROVA PROVA</h2>
-  </div>
+    <h1 class="" >Yu-Gi-Oh Api</h1>
+  </header>
 </template>
 
 
 <style lang="scss">
-h2 {
-    color: aqua;
+header {
+    background-color: white;
+    max-width: 1500px;
+    margin: 0 auto;
+    padding: 20px;
+    display: flex;
+    align-items: center;
+
+    img {
+        width: 150px;
+    }
+
+    ;
 }
 </style>
